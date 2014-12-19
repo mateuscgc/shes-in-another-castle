025944aadd37ddff0c602fe911873d82749139aa
Mateus <mateus_cgc@hotmail.com>
2014-12-19 17:34:58 -0300

    modularização do código

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function Character(name, strength, skill, defense, resistance, firePower) {
+//Classe de Personagem
+var Character = function (name, strength, skill, defense, resistance, firePower) {
     'use strict';
     this.name = name;
     this.strength = strength;
@@ -8,55 +9,74 @@ function Character(name, strength, skill, defense, resistance, firePower) {
     this.firePower = firePower;
     this.initialHP = resistance > 0 ? this.resistance * 5 : 1;
     this.HP = this.initialHP;
-    
-    this.attacks = function (strength) {
-        var i, dmg = 0;
-        for (i = 1; i <= strength; i += 1) {
-            dmg += strength * Math.ceil(Math.random() * 6);
-        }
-        return dmg;
-    };
+};
+Character.prototype.attacks = function () {
+    'use strict';
+    var i, dmg = 0;
+    for (i = 1; i <= this.strength; i += 1) {
+        dmg += this.strength * Math.ceil(Math.random() * 6);
+    }
+    return dmg;
+};
         
-    this.defends = function (defense) {
-        var i, def = 0;
-        for (i = 1; i <= defense; i += 1) {
-            def += defense * Math.ceil(Math.random() * 6);
-        }
-        return def;
-    };
-}
+Character.prototype.defends = function () {
+    'use strict';
+    var i, def = 0;
+    for (i = 1; i <= this.defense; i += 1) {
+        def += this.defense * Math.ceil(Math.random() * 6);
+    }
+    return def;
+};
 
-
-var attack = function (attacker, defender) {
+Character.prototype.display = function () {
+    'use strict';
+    var container, char, name;
+    container = document.querySelector('body');
+    char = document.createElement('div');
     
+    name = document.createElement('h1');
+    name.innerHTML = 'Nome: ' + this.name;
+    
+    char.appendChild(name);
+    container.appendChild(char);
+};
+
+
+// Classe de Combate
+var Battle = function (p1, p2) {
+    'use strict';
+    this.p1 = p1;
+    this.p2 = p2;
+};
+
+Battle.prototype.attack = function (attacker, defender) {
     'use strict';
     var i, totalDMG = 0;
-    
+
     // calcula dano
-    totalDMG += attacker.attacks(attacker.strength);
-    
+    totalDMG += attacker.attacks();
+
     //calcula defesa 
-    totalDMG -= defender.defends(defender.defense);
-    
+    totalDMG -= defender.defends();
+
     if (totalDMG > 0) {
         defender.HP -= totalDMG;
     }
 };
-
-var fight = function (p1, p2) {
     
+Battle.prototype.fight = function (p1, p2) {
     'use strict';
     var turn = 0;
-    
+
     while (p1.HP > 0 && p2.HP > 0) {
         if (turn === 0) {
-            attack(p1, p2);
+            this.attack(p1, p2);
         } else {
-            attack(p2, p1);
+            this.attack(p2, p1);
         }
         turn = (turn + 1) % 2;
     }
-    
+
     if (p1.HP <= 0) {
         window.console.log(p1.name + ' died from sickness!');
         return p2;
@@ -66,56 +86,61 @@ var fight = function (p1, p2) {
     }
 };
 
-var newCharacter = function (name, points) {
+var newPlayer = function (name, points) {
     'use strict';
     var str, skill, def, res, FP, c;
-    
+
     if (points === undefined) {
         points = 10;
     }
-   
+
     if (points >= 5) {
         str = Math.ceil(Math.random() * 5);
     } else {
         str = Math.ceil(Math.random() * points);
     }
     points -= str;
-    
+
     if (points >= 5) {
         skill = Math.ceil(Math.random() * 5);
     } else {
         skill = Math.ceil(Math.random() * points);
     }
     points -= skill;
-    
+
     if (points >= 5) {
         def = Math.ceil(Math.random() * 5);
     } else {
         def = Math.ceil(Math.random() * points);
     }
     points -= def;
-    
+
     if (points >= 5) {
         res = Math.ceil(Math.random() * 5);
     } else {
         res = Math.ceil(Math.random() * points);
     }
     points -= res;
-    
+
     if (points >= 5) {
         FP = Math.ceil(Math.random() * 5);
     } else {
         FP = Math.ceil(Math.random() * points);
     }
     points -= FP;
-    
+
     c = new Character(name, str, skill, def, res, FP);
-    
+
     return c;
 };
 
-var newGame = function (name1, name2, points1, points2) {
+
+var Game = function (name1, name2, points1, points2) {
     'use strict';
+    var b, p1, p2;
+    p1 = newPlayer(name1, points1);
+    p2 = newPlayer(name2, points2);
+    
     if (points1 === undefined) {
         points1 = 10;
     }
@@ -124,6 +149,9 @@ var newGame = function (name1, name2, points1, points2) {
         points2 = 10;
     }
     
-    return fight(newCharacter(name1, points1), newCharacter(name2, points2));
+    b = new Battle(name1, name2, points1, points2);
+    p1.display();
+    
+    return b.fight(p1, p2);
     
 };
