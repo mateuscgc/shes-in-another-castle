890e4b544e9c549b7eec5829afb1828e5ecf8642
Mateus <mateus_cgc@hotmail.com>
2014-12-19 16:38:28 -0300

    criada classe de character

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+function Character(name, strength, skill, defense, resistance, firePower) {
+    'use strict';
+    this.name = name;
+    this.strength = strength;
+    this.skill = skill;
+    this.defense = defense;
+    this.resistance = resistance;
+    this.firePower = firePower;
+    this.initialHP = resistance > 0 ? this.resistance * 5 : 1;
+    this.HP = this.initialHP;
+    
+    this.attacks = function (strength) {
+        var i, dmg = 0;
+        for (i = 1; i <= strength; i += 1) {
+            dmg += strength * Math.ceil(Math.random() * 6);
+        }
+        return dmg;
+    };
+        
+    this.defends = function (defense) {
+        var i, def = 0;
+        for (i = 1; i <= defense; i += 1) {
+            def += defense * Math.ceil(Math.random() * 6);
+        }
+        return def;
+    };
+}
+
 
 var attack = function (attacker, defender) {
     
@@ -5,7 +33,7 @@ var attack = function (attacker, defender) {
     var i, totalDMG = 0;
     
     // calcula dano
-    totalDMG += attacker.hits(attacker.attack);
+    totalDMG += attacker.attacks(attacker.strength);
     
     //calcula defesa 
     totalDMG -= defender.defends(defender.defense);
@@ -30,94 +58,63 @@ var fight = function (p1, p2) {
     }
     
     if (p1.HP <= 0) {
-        window.console.log(p1.title + ' died from sickness!');
+        window.console.log(p1.name + ' died from sickness!');
         return p2;
     } else {
-        window.console.log(p2.title + ' died by a knife!');
+        window.console.log(p2.name + ' died by a knife!');
         return p1;
     }
 };
 
 var newCharacter = function (name, points) {
     'use strict';
+    var str, skill, def, res, FP, c;
+    
     if (points === undefined) {
         points = 10;
     }
-    
-    var character = {
-        title: name,
-        attack: 0,
-        skill: 0,
-        defense: 0,
-        resistance: 0,
-        firePower: 0,
-        HP: 0,
-        initialHP: 0,
-        
-        hits: function(attack) {
-            var i, dmg = 0;
-            for (i = 1; i <= attack; i += 1){
-                dmg += attack * Math.ceil(Math.random() * 6);
-            }
-            return dmg;
-        },
-        
-        defends: function (defense) {
-            var i, def = 0;
-            for (i = 1; i <= defense; i += 1){
-                def += defense * Math.ceil(Math.random() * 6);
-            }
-            return def;
-        }
-    };
+   
+    if (points >= 5) {
+        str = Math.ceil(Math.random() * 5);
+    } else {
+        str = Math.ceil(Math.random() * points);
+    }
+    points -= str;
     
     if (points >= 5) {
-        character.attack = Math.ceil(Math.random() * 5);
+        skill = Math.ceil(Math.random() * 5);
     } else {
-        character.attack = Math.ceil(Math.random() * points);
+        skill = Math.ceil(Math.random() * points);
     }
-    points -= character.attack;
+    points -= skill;
     
     if (points >= 5) {
-        character.skill = Math.ceil(Math.random() * 5);
+        def = Math.ceil(Math.random() * 5);
     } else {
-        character.skill = Math.ceil(Math.random() * points);
+        def = Math.ceil(Math.random() * points);
     }
-    points -= character.skill;
+    points -= def;
     
     if (points >= 5) {
-        character.defense = Math.ceil(Math.random() * 5);
+        res = Math.ceil(Math.random() * 5);
     } else {
-        character.defense = Math.ceil(Math.random() * points);
+        res = Math.ceil(Math.random() * points);
     }
-    points -= character.defense;
+    points -= res;
     
     if (points >= 5) {
-        character.resistance = Math.ceil(Math.random() * 5);
+        FP = Math.ceil(Math.random() * 5);
     } else {
-        character.resistance = Math.ceil(Math.random() * points);
+        FP = Math.ceil(Math.random() * points);
     }
-    points -= character.resistance;
+    points -= FP;
     
-    if (points >= 5) {
-        character.firePower = Math.ceil(Math.random() * 5);
-    } else {
-        character.firePower = Math.ceil(Math.random() * points);
-    }
-    points -= character.firePower;
+    c = new Character(name, str, skill, def, res, FP);
     
-    if (character.resistance === 0) {
-        character.initialHP = 1;
-    } else {
-        character.initialHP = character.resistance * 5;
-    }
-    character.HP = character.initialHP;
-    
-    return character;
-    
+    return c;
 };
 
-var newGame = function(name1, name2, points1, points2) {
+var newGame = function (name1, name2, points1, points2) {
     'use strict';
     if (points1 === undefined) {
         points1 = 10;
