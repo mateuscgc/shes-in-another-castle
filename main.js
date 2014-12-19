442b744891df423d02159f6b4a838989c0098685
Mateus <mateus_cgc@hotmail.com>
2014-12-19 11:28:38 -0300

    criado arrays dos jogadores

--- main.js
+++ main.js
@@ -6,4 +6,7 @@ var fight = function (atk, arm) {
     return 'Second';
 };
 
-    
+var p1 = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5)];
+p1[3] = p1[2] * 5;
+var p2 = [Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5), Math.ceil(Math.random() * 5)];
+p2[3] = p2[2] * 5;
